fba6d36a77a8f0c9c589bc8d15f326c8b37f9190
officiallyrix <djlyrix22@gmail.com>
2026-04-13 02:15:37 +0100

    Create investment portfolio page for viewing stocks, funds, and managing investment portfolios

---
+++ frontend/pages/investments.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Investments = () => {
+    return (
+        <div>
+            <h1>Investment Portfolio</h1>
+            <h2>Your Stocks</h2>
+            {/* Code to display stocks goes here */}
+            <h2>Your Funds</h2>
+            {/* Code to display funds goes here */}
+            <h2>Manage Investment Portfolio</h2>
+            {/* Code to manage investment portfolios goes here */}
+        </div>
+    );
+};
+
+export default Investments;
